57247b7ea29681a59d0d17aa5b23dce7c8ffdcc2
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-01-13 00:35:59 +0100

    Fix intermediate.lab01.

--- labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterStatus.scala
+++ labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterStatus.scala
@@ -1,2 +1,50 @@
 package org.scalalabs.intermediate.lab01
 
+import scala.xml._
+import org.joda.time._
+import format.{DateTimeFormatter, DateTimeFormat}
+import java.util.Locale
+
+case class TwitterStatus(
+    id: Long,
+    inReplyToStatusId: Option[Long],
+    inReplyToUserId: Option[Long],
+    truncated: Boolean,
+    favorited: Boolean,
+    text: String,
+    createdAt: DateTime,
+    user: TwitterUser
+)
+
+object TwitterStatus {
+
+  def apply(node: Node): TwitterStatus = {
+
+    def text(attribute: String) = (node \ attribute).text
+    def boolean(attribute: String) = text(attribute).toBoolean
+    def long(attribute: String) = text(attribute).toLong
+    def option(attribute: String): Option[Long] = {
+      val value: String = text(attribute)
+      if (value.isEmpty) None
+      else Some(value.toLong)
+    }
+
+    def dateFormat(attribute: String): DateTime = {
+      val dtf: DateTimeFormatter = DateTimeFormat.forPattern("EE MMM dd HH:mm:ss Z yyyy").withLocale(Locale.US)
+      dtf.parseDateTime(text(attribute))
+    }
+
+    new TwitterStatus(
+      long("id"),
+      option("in_reply_to_status_id"),
+      option("in_reply_to_user_id"),
+      boolean("truncated"),
+      boolean("favorited"),
+      text("text"),
+      dateFormat("created_at"),
+      TwitterUser((node \ "user").head)
+    )
+  }
+
+
+}
--- labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterUser.scala
+++ labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterUser.scala
@@ -1,3 +1,52 @@
 package org.scalalabs.intermediate.lab01
 
+import scala.xml._
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import java.util.Locale
 
+case class TwitterUser(
+  id: Long,
+  name: String,
+  screen_name: String,
+  description: String,
+  location: String,
+  url: String,
+  profileImageUrl: String,
+  statusesCount: Int,
+  friendsCount: Int,
+  followersCount: Int
+)
+
+object TwitterUser {
+
+  def apply(node: Node): TwitterUser = {
+    def text(attribute: String) = (node \ attribute).text
+    def boolean(attribute: String) = text(attribute).toBoolean
+    def long(attribute: String) = text(attribute).toLong
+    def int(attribute: String) = text(attribute).toInt
+    def option(attribute: String): Option[Long] = {
+      val value: String = text(attribute)
+      if (value.isEmpty) None
+      else Some(value.toLong)
+    }
+
+    def dateFormat(attribute: String): DateTime = {
+      val dtf: DateTimeFormatter = DateTimeFormat.forPattern("EE MMM dd HH:mm:ss Z yyyy").withLocale(Locale.US)
+      dtf.parseDateTime(text(attribute))
+    }
+
+    new TwitterUser(
+      long("id"),
+      text("name"),
+      text("screen_name"),
+      text("description"),
+      text("location"),
+      text("url"),
+      text("profile_image_url"),
+      int("statuses_count"),
+      int("friends_count"),
+      int("followers_count")
+    )
+  }
+}
